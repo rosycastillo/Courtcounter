e8b075e00bf6b8b20ad5fc3bdef95c2cee3d9da2
rosy castilllo <rosyycastillo23@gmail.com>
2016-11-14 15:04:57 -0800

    please give me an A
    I HAVE A TON OF ERRORS

--- app/src/main/java/com/example/student/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/student/courtcounter/MainActivity.java
@@ -46,9 +46,9 @@ public class MainActivity extends AppCompatActivity {
 
         gameOver = new AlertDialog.Builder(MainActivity.this).create();
 
-        soundFX1 = MediaPlayer.create(this, R.raw.up);
-        soundFX2 = MediaPlayer.create(this, R.raw.down);
-        soundFX3 = MediaPlayer.create(this, R.raw.reset);
+        soundFX1 = MediaPlayer.create(this, R.raw.a);
+        soundFX2 = MediaPlayer.create(this, R.raw.b);
+        soundFX3 = MediaPlayer.create(this, R.raw.c);
     }
 
     public void click1(View view) {
